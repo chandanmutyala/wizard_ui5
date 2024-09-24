f21246593a39231766c621d38d60f95e34e4a085
vijay amararapu <amararapu55@gmail.com>
2024-09-24 17:11:38 +0000

    initial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+	'sap/ui/model/json/JSONModel'
+],
+function (Controller, JSONModel) {
+    "use strict";
+
+    return Controller.extend("wizards.controller.View1", {
+        onInit: function () {
+
+        },
+        onCategoryChange: function (oEvent) {
+            // Get the selected key
+            var selectedKey = oEvent.getSource().getSelectedKey();
+            
+            // Get references to the panels
+            var oProjectPanel = this.byId("ProjectForm");
+            var oProcurementPanel = this.byId("ProcurementForm");
+            
+            // Set visibility based on the selected key
+            if (selectedKey === "ProjectForm") {
+                oProjectPanel.setVisible(true);
+                oProcurementPanel.setVisible(false);
+            } else if (selectedKey === "ProcurementForm") {
+                oProjectPanel.setVisible(false);
+                oProcurementPanel.setVisible(true);
+            }
+        }
+    });
+});
